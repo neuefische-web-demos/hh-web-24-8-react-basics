2a70c2a8bec22773e1170bc92a7af5e810753e00
Niklas Röttger <niklas.roettger@outlook.de>
2024-09-30 15:18:02 +0200

    add session code

--- src/VanillaApp.js
+++ src/VanillaApp.js
@@ -1,5 +1,21 @@
 export default function VanillaApp() {
-  const div = document.createElement("div");
-  div.innerHTML = "Vanilla JS";
-  return div;
+  const appContainer = document.createElement("div");
+
+  appContainer.append(AppButton());
+  appContainer.append(AppButton());
+  appContainer.append(AppButton());
+
+  return appContainer;
+}
+
+function AppButton() {
+  const appButton = document.createElement("button");
+  // type
+  appButton.type = "button";
+  // textContent
+  appButton.textContent = "Klick mich";
+  // event listener
+  appButton.addEventListener("click", () => console.log("Hello World"));
+
+  return appButton;
 }
